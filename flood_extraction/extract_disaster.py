cdfc96191a43af262fffd883764a6e57ce2a7d5e
gjang <gjang@kaist.ac.kr>
2017-02-23 10:43:07 +0000

    update the file paths
    update file paths
    applying the specific and generic terms for disaster

--- flood_extraction/extract_disaster.py
+++ flood_extraction/extract_disaster.py
@@ -13,7 +13,7 @@ import re
 
 #path = 'http://recoin.cloudapp.net/data/news_theguardian.json'
 #path ='Git/disaster_datathon/floods.json'
-path ='851_guardian_files.json'
+path ='../news_source_files/851_guardian_files.json'
 #docs = [json.loads(l) for l in open(path)]
 docs = []
 #response = urllib2.urlopen(path)
@@ -33,25 +33,36 @@ df=pd.DataFrame(docs)
 #    idx+=1
 #df
 
+#
 
-# In[76]:
-
-term_file = 'FloodTerms.txt'
+term_file = 'FloodTerms_filtered.txt'
 gw = []
-fw = []
-specific=False
+sfw = []
+gfw = []
+words={}
+#specific=False
+sec = ""
 for line in open(term_file):
-    if line.strip().endswith("Words"):
-        if 'Flooding' in line:
-            specific = True
-        continue
+    if line.startswith('['):
+        sec = line.strip()[1:-1]
+        #print sec
+    #if line.strip().endswith("Words"):
+    #    if 'Flooding' in line:
+    #        specific = True
+    #    continue
     elif len(line.strip()) == 0 :
         continue
     else:
-        if specific:
-            fw.append(line.strip().lower())
-        else:
-            gw.append(line.strip().lower())
+        if sec not in words:
+            words[sec]=[]
+        words[sec].append(line.strip().lower())
+        #if specific:
+        #    fw.append(line.strip().lower())
+        #else:
+        #    gw.append(line.strip().lower())
+gw = words['Generic Words']
+sfw = words['Specific Flooding Words']
+gfw = words['Generic Flooding Words']
 #print gw
 #print "|".join(gw)
 #print "|".join(fw)
@@ -64,25 +75,38 @@ for line in open(term_file):
 #terms_re = "|".join(terms)
 match_title=[]
 match_cont=[]
-idx = 0
+th = 10
+th_prop = 0.01
 for title in df['title'] : 
-    matches_gw = {x for x in gw if x in title}
-    matches_fw = {x for x in fw if x in title}
-    match_title.append((len(matches_gw),len(matches_fw)))
+    matches_gw = {x for x in gw if x in title.lower()}
+    matches_gfw = {x for x in gfw if x in title.lower()}
+    matches_sfw = {x for x in sfw if x in title.lower()}
+    match_title.append((len(matches_sfw),len(matches_gw),len(matches_gfw)))
     #print idx,len(matches_gw),len(matches_fw)
-    idx+=1 
-idx = 0
 for cont in df['content']:
-    matches_gw = {x for x in gw if x in cont}
-    matches_fw = {x for x in fw if x in cont}
-    match_cont.append((len(matches_gw),len(matches_fw)))
+    matches_gw = {x for x in gw if x in cont.lower()}
+    matches_gfw = {x for x in gfw if x in cont.lower()}
+    matches_sfw = {x for x in sfw if x in cont.lower()}
+    match_cont.append((len(matches_sfw),len(matches_gw),len(matches_gfw)))
     #print idx,len(matches_gw),len(matches_fw)
-    idx+=1
+cnt = 0
 for i in xrange(len(match_cont)) :
-    cnt_title = match_title[i][0]+match_title[i][1]
-    cnt_cont = match_cont[i][0] + match_cont[i][1]
-    if cnt_title+cnt_cont > 10:
-        print i,match_title[i],match_cont[i]
+    if match_title[i][0]+match_cont[i][0] > 0:
+        print i
         print df.ix[i]
-    #print len(re.findall("|".join(gw),cont))
+    elif 'flood' in df['content'][i].lower() or 'flood' in df['title'][i].lower():
+        
+        cnt_title = match_title[i][1]+match_title[i][2]
+        cnt_cont = match_cont[i][1]+match_cont[i][2]
+        if cnt_title+cnt_cont > 0:
+            prop_title = cnt_title*1.0/len(df['title'][i].split())
+            prop_cont = cnt_cont*1.0/len(df['content'][i].split())
+            #print i,prop_title, prop_cont
+            if prop_title > 0.1 and prop_cont > th_prop:
+                print i,prop_title,prop_cont
+                print df.ix[i]
+            if cnt_title+cnt_cont>th:
+                print i,cnt_title,cnt_cont
+                print df.ix[i]
+
 
