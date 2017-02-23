549a47f23ec9841bea06f76e714733ca128fda7c
gjang <gjang@kaist.ac.kr>
2017-02-23 12:02:28 +0000

    update extraction_disaster.py
    applying publication_date_differences

--- flood_extraction/extract_disaster.py
+++ flood_extraction/extract_disaster.py
@@ -1,112 +1,93 @@
 
 # coding: utf-8
 
-# In[34]:
-
 import pandas as pd
 import json
 import urllib2
 import re
 
 
-# In[99]:
-
-#path = 'http://recoin.cloudapp.net/data/news_theguardian.json'
-#path ='Git/disaster_datathon/floods.json'
-path ='../news_source_files/851_guardian_files.json'
+#path ='../news_source_files/851_guardian_files.json'
+path = '../news_source_files/1275_guardian.json'
+docs = json.load(open(path))
 #docs = [json.loads(l) for l in open(path)]
-docs = []
-#response = urllib2.urlopen(path)
-idx = 0
-for l in open(path):
-    #print l
-    docs.append(json.loads(l.strip()))
-    #print docs
-    #if len(docs) == 2:
-    #    break
-
-#df=pd.read_json(path)
 df=pd.DataFrame(docs)
-#idx = 0
-#for t in df.title:
-#    print idx,t
-#    idx+=1
-#df
-
-#
 
 term_file = 'FloodTerms_filtered.txt'
 gw = []
 sfw = []
 gfw = []
 words={}
-#specific=False
 sec = ""
 for line in open(term_file):
     if line.startswith('['):
         sec = line.strip()[1:-1]
-        #print sec
-    #if line.strip().endswith("Words"):
-    #    if 'Flooding' in line:
-    #        specific = True
-    #    continue
     elif len(line.strip()) == 0 :
         continue
     else:
         if sec not in words:
             words[sec]=[]
         words[sec].append(line.strip().lower())
-        #if specific:
-        #    fw.append(line.strip().lower())
-        #else:
-        #    gw.append(line.strip().lower())
 gw = words['Generic Words']
 sfw = words['Specific Flooding Words']
 gfw = words['Generic Flooding Words']
-#print gw
-#print "|".join(gw)
-#print "|".join(fw)
 
-
-# In[108]:
-
-#terms = ['disaster','flood','earthquake', 'More']
-
-#terms_re = "|".join(terms)
 match_title=[]
 match_cont=[]
 th = 10
-th_prop = 0.01
+th_prop_cont = 0.001
+th_prop_title = 0.05
 for title in df['title'] : 
     matches_gw = {x for x in gw if x in title.lower()}
     matches_gfw = {x for x in gfw if x in title.lower()}
     matches_sfw = {x for x in sfw if x in title.lower()}
     match_title.append((len(matches_sfw),len(matches_gw),len(matches_gfw)))
-    #print idx,len(matches_gw),len(matches_fw)
 for cont in df['content']:
     matches_gw = {x for x in gw if x in cont.lower()}
     matches_gfw = {x for x in gfw if x in cont.lower()}
     matches_sfw = {x for x in sfw if x in cont.lower()}
     match_cont.append((len(matches_sfw),len(matches_gw),len(matches_gfw)))
-    #print idx,len(matches_gw),len(matches_fw)
 cnt = 0
+disasters=[]
 for i in xrange(len(match_cont)) :
+    isDisaster=False
     if match_title[i][0]+match_cont[i][0] > 0:
-        print i
-        print df.ix[i]
+        #print i
+        #print df.ix[i]
+        isDisaster=True
+        #disasters.append(i)
     elif 'flood' in df['content'][i].lower() or 'flood' in df['title'][i].lower():
-        
         cnt_title = match_title[i][1]+match_title[i][2]
         cnt_cont = match_cont[i][1]+match_cont[i][2]
         if cnt_title+cnt_cont > 0:
             prop_title = cnt_title*1.0/len(df['title'][i].split())
             prop_cont = cnt_cont*1.0/len(df['content'][i].split())
             #print i,prop_title, prop_cont
-            if prop_title > 0.1 and prop_cont > th_prop:
-                print i,prop_title,prop_cont
-                print df.ix[i]
-            if cnt_title+cnt_cont>th:
-                print i,cnt_title,cnt_cont
-                print df.ix[i]
-
-
+            if (prop_title > th_prop_title and prop_cont > th_prop_cont) or prop_cont > th_prop_cont*2:
+                #print i,prop_title,prop_cont,cnt_title,cnt_cont
+                #print df.ix[i]
+                isDisaster=True
+            elif cnt_title+cnt_cont>th:
+                #print i,cnt_title,cnt_cont,prop_title,prop_cont
+                #print df.ix[i]
+                isDisaster=True
+    if isDisaster:
+        res = sorted(map(int,re.findall("20[0-1][0-9]|19[0-9][0-9]",df.ix[i]['content'])))
+        publication_year = int(df.ix[i]['publication_date'].split('-')[0])
+        if len(res)==0 or (publication_year-res[-1] <10 and publication_year-res[-1] >=0):
+            disasters.append(i)
+print disasters
+"""            
+conts = df.iloc[disasters]['content']
+idx = 0
+for cont in conts:
+    res = sorted(map(int,re.findall("20[0-1][0-9]|19[0-9][0-9]",cont)))
+    selected=df.ix[disasters[idx]]
+    publication_year = int(selected['publication_date'].split('-')[0])
+    if len(res)==0 or (publication_year-res[-1] <10 and publication_year-res[-1] >=0):
+        print selected['_id'],selected['publication_date'],res
+    #else:
+    #    print selected
+    idx+=1
+print idx
+"""
