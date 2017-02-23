798d354633a648d069b0c4b1714da73f4e3632fb
Sami Kanza <samikanza@Samis-MacBook.local>
2017-02-23 15:56:47 +0000

    Latest version of dashboard with locationd data

--- public_html/diaster_dashboard.py
+++ public_html/diaster_dashboard.py
@@ -1,55 +1,101 @@
-# write-html-2-mac.py
 import webbrowser
 import pandas as pd
+import time
 import json
 import urllib2
 import re
+import math
 
 f = open('dashboard.html','w')
-path ='../flood_extraction/851_guardian_files.json'
-docs = []
+towns_and_cities = []
+countries = []
 
-idx = 0
-for l in open(path):
-    #print l
-    docs.append(json.loads(l.strip()))
+with open('../location_extraction/uk_towns_and_cities.txt') as towns_and_cities_file:    
+    read_data = towns_and_cities_file.read()
+towns_and_cities=[re.sub(r'\(.+?\)\s*', '', x) for x in read_data.split("\n") if len(x) > 0]
+towns_and_cities_file.close()
 
+print(str(towns_and_cities))
+
+with open('../location_extraction/countries.txt') as countries_file:    
+    countries = countries_file.read()
+countries = [z.replace(" {Republic}", "") for z in countries.split("\n")]
+countries_file.close()
+
+def extract_location(title,content):
+	dictionary = {}	
+	most_affected_places = {}
+	dictionary[title] = []
+	content = content.replace(".", "").replace(",", "").split(" ") + title.split(" ")
+
+	caps = [i for i in content if i.istitle()]
+	for word in caps:
+		if word in towns_and_cities:
+			if word in towns_and_cities: 
+				most_affected_places = __add_to_dict_if_not_in(most_affected_places, word)
+		elif word in countries:
+			if word in most_affected_places:
+				most_affected_places = __add_to_dict_if_not_in(most_affected_places, word)
+
+	list_of_lists  = [[k, most_affected_places[k]] for k in most_affected_places]
+	
+	list_of_lists = sorted(list_of_lists, key = lambda x: (-x[1]))
+	dictionary[title] = [x[0] for x in list_of_lists]
+	
+	return dictionary
+
+def __add_to_dict_if_not_in(dictionary, word):
+	if (len(dictionary) == 0) or (word not in dictionary.keys()):
+		dictionary[word] = 1
+	else:
+		dictionary[word] += 1
+
+	return dictionary
+
+##################     extracting flood data         ########################
+path = '../news_source_files/1275_guardian.json'
+docs = json.load(open(path))
 df=pd.DataFrame(docs)
 
-term_file = '../flood_extraction/FloodTerms.txt'
+term_file = '../flood_extraction/FloodTerms_filtered.txt'
 gw = []
-fw = []
-specific=False
+sfw = []
+gfw = []
+words={}
+sec = ""
 for line in open(term_file):
-    if line.strip().endswith("Words"):
-        if 'Flooding' in line:
-            specific = True
-        continue
+    if line.startswith('['):
+        sec = line.strip()[1:-1]
     elif len(line.strip()) == 0 :
         continue
     else:
-        if specific:
-            fw.append(line.strip().lower())
-        else:
-            gw.append(line.strip().lower())
+        if sec not in words:
+            words[sec]=[]
+        words[sec].append(line.strip().lower())
+gw = words['Generic Words']
+sfw = words['Specific Flooding Words']
+gfw = words['Generic Flooding Words']
 
 match_title=[]
 match_cont=[]
-idx = 0
+th = 10
+th_prop_cont = 0.01
+th_prop_title = 0.1
 for title in df['title'] : 
-    matches_gw = {x for x in gw if x in title}
-    matches_fw = {x for x in fw if x in title}
-    match_title.append((len(matches_gw),len(matches_fw)))
-    #print idx,len(matches_gw),len(matches_fw)
-    idx+=1 
-idx = 0
+    matches_gw = {x for x in gw if x in title.lower()}
+    matches_gfw = {x for x in gfw if x in title.lower()}
+    matches_sfw = {x for x in sfw if x in title.lower()}
+    match_title.append((len(matches_sfw),len(matches_gw),len(matches_gfw)))
 for cont in df['content']:
-    matches_gw = {x for x in gw if x in cont}
-    matches_fw = {x for x in fw if x in cont}
-    match_cont.append((len(matches_gw),len(matches_fw)))
-    #print idx,len(matches_gw),len(matches_fw)
-    idx+=1
+    matches_gw = {x for x in gw if x in cont.lower()}
+    matches_gfw = {x for x in gfw if x in cont.lower()}
+    matches_sfw = {x for x in sfw if x in cont.lower()}
+    match_cont.append((len(matches_sfw),len(matches_gw),len(matches_gfw)))
+cnt = 0
+disasters=[]
 
+
+##################     creating HTML         ########################
 message = """
 <!DOCTYPE html>
 <html>
@@ -70,15 +116,33 @@ message = """
 				</tr>"""
 
 for i in xrange(len(match_cont)) :
-    cnt_title = match_title[i][0]+match_title[i][1]
-    cnt_cont = match_cont[i][0] + match_cont[i][1]
-    if cnt_title+cnt_cont > 10:
+    isDisaster=False
+    if match_title[i][0]+match_cont[i][0] > 0:
+        isDisaster=True
+    elif 'flood' in df['content'][i].lower() or 'flood' in df['title'][i].lower():
+        cnt_title = match_title[i][1]+match_title[i][2]
+        cnt_cont = match_cont[i][1]+match_cont[i][2]
+        if cnt_title+cnt_cont > 0:
+            prop_title = cnt_title*1.0/len(df['title'][i].split())
+            prop_cont = cnt_cont*1.0/len(df['content'][i].split())
+            if (prop_title > th_prop_title and prop_cont > th_prop_cont) or prop_cont > th_prop_cont*2:
+                isDisaster=True
+            elif cnt_title+cnt_cont>th:
+                isDisaster=True
+    if isDisaster:
+        res = sorted(map(int,re.findall("20[0-1][0-9]|19[0-9][0-9]",df.ix[i]['content'])))
+        publication_year = int(df.ix[i]['publication_date'].split('-')[0])
+        if len(res)==0 or (publication_year-res[-1] <10 and publication_year-res[-1] >=0):
+            disasters.append(i)
+        location = {}
+        location = extract_location(df.ix[i]['title'],df.ix[i]['content'])
+
     	message+="""
     		<tr>
     			<td>Flood</td>
-    			<td>Serious</td>
+    			<td><img class="traffic-light" src="images/red-traffic-light.png"/></td>
     			<td><a target="_blank" href='""" + str(df.ix[i]['url']) + """'>""" + str(df.ix[i]['url']) + """</a</td>
-    			<td>Location</td>
+    			<td>""" + str(location[df.ix[i]['title']])[1:-1] + """</td>
 				<td>Resiliance</td>
 			</tr>"""
 
